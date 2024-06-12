ec66235e3857a6ea57b84f05ab1a02918490f8b5
GurTati <tatiana.gurova.j@gmail.com>
2024-06-12 19:17:02 +0100

    add some logic

--- baker.py
+++ baker.py
@@ -1,2 +1,7 @@
 def cake(available, recipe):
-    return 0
+    portions = []
+    for ingredient_name, ingredient_value in recipe.items():
+        available_value = available[ingredient_name]
+        portion_value = available_value // ingredient_value
+        portions.append(portion_value)
+    return min(portions)

4f56ff9470d53a11a74563fc3abf82ee21a6a531
Magdalena Piatkowska <magda.piatk@gmail.com>
2024-06-12 19:40:49 +0100

    Adding black, excepting .venv in .gitignore, refining cake()

--- baker.py
+++ baker.py
@@ -1,7 +1,8 @@
 def cake(available, recipe):
     portions = []
     for ingredient_name, ingredient_value in recipe.items():
-        available_value = available[ingredient_name]
+        available_value = available.get(ingredient_name, 0)
         portion_value = available_value // ingredient_value
         portions.append(portion_value)
+        
     return min(portions)
--- test_baker.py
+++ test_baker.py
@@ -1,12 +1,14 @@
 import pytest
-from  baker import cake
+from baker import cake
 
-# STEP 1: write the production code to ensure the test passes. 
+
+# STEP 1: write the production code to ensure the test passes.
 def test_returns_0_if_no_ingredients_available():
     available = {}
     recipe = {"flour": 400}
     max_cakes = cake(available, recipe)
     assert max_cakes == 0
+
 
 # STEP 2: now it's time to write your own tests.
 def test_returns_2_if_ingredients_are_enough():
